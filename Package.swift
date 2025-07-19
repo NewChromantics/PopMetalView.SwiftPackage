f06b933faa6426c36150e5b71e310dbbe5304940
Graham Reeves <graham@grahamreeves.com>
2025-07-19 16:14:58 +0100

    ios fixes

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
 	name: "PopMetalView",
 	
 	platforms: [
-		.iOS(.v15),
+		.iOS(.v17),		//	stroke/fill styles in ios17
 		//.macOS(.v10_15)
 		//.macOS(.v12)	//	for .overlay{} - revert to zstack if this is a problem
 		.macOS(.v14)	//	for gizmo stroke/fill style
